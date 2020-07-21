683028e5554d30eb3a4dcee2f1bd59123dfffa00
Vishnu Namboodiri <vishnu.nks@niser.ac.in>
2020-07-21 19:54:08 +0530

    init

--- itsie/old/test2.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python3
-
-import asyncio
-import databaser
-
-async def add2db():
-    await databaser.connect("https://mermin.com", "Achscroft Mermin", "Cengage publications")
-
-asyncio.run(add2db())
